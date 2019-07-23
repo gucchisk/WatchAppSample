933625c63ba17f1f13ce7f4ed2347a563e3aa45e
gucchisk <gucchi_sk@yahoo.co.jp>
2019-07-24 03:11:17 +0900

    Add notification

--- WatchAppSample WatchKit Extension/InterfaceController.swift
+++ WatchAppSample WatchKit Extension/InterfaceController.swift
@@ -8,10 +8,38 @@
 
 import WatchKit
 import Foundation
-
+import UserNotifications
 
 class InterfaceController: WKInterfaceController {
+    @IBAction func onGoodButton() {
+        let content = UNMutableNotificationContent()
+        content.title = "title"
+        content.body = "body"
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
 
+    }
+    @IBAction func onBadButton() {
+    }
+
+    override func handleAction(withIdentifier identifier: String?, for localNotification: UILocalNotification) {
+        print("local notification")
+    }
+    
+    override func handleAction(withIdentifier identifier: String?, forRemoteNotification remoteNotification: [AnyHashable : Any]) {
+        if identifier == "nazo" {
+            let alertAction = WKAlertAction.init(title: "???", style: .default, handler: {
+            })
+            self.presentAlert(withTitle: "nazo", message: "???", preferredStyle: .alert, actions: [alertAction])
+        }
+    }
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         // Configure interface objects here.
